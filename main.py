39e95ccb3b62661215590b5080335bce62a76522
vladimirogulo <vovandrex@gmail.com>
2022-11-30 17:33:35 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+from typing import Optional, List
+from fastapi import FastAPI,Path,Query
+from pydantic import BaseModel
+
+app = FastAPI(
+    title="AppFastApi",
+    description="My first web app",
+    version="0.0.1",
+    contact={"name": "Vladimir"}  
+)
+
+users = []
+
+class User(BaseModel):
+    email: str
+    is_active: bool
+    bio: Optional[str]
+
+
+@app.get("/users", response_model=List[User])
+async def get_users():
+    return users
+
+@app.post("/users")
+async def create_user(user: User):
+    users.append(user)
+    return "Success"
+
+
+@app.get(f"/users/{id}")
+async def get_user(id: int = Path(..., description="The id of user you want", gt=2),
+                   q: str = Query(None, max_length=5)):
+    return {"user":users[id], "query":q}
